2e001d2930c11e6add113f6242038bb16f0b62bc
Eli Tosta Junior <eli_gtj@hotmail.com>
2024-09-27 15:22:01 -0300

    atualização banco de dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/projeto-base/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/elitostajunior/portfolio-js-dio/refs/heads/master/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
